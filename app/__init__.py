6bab50d2fc54b9fb55d7021d7d9d9886b102e3bf
mmikri <mirine.kr.97@gmail.com>
2024-03-05 23:44:49 +0900

    20240305 add Initial gui

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
 # __init__.py
 
 # Initializes and defines package version.
-__version__ = "1.0.0"
+__version__ = "2.0.0"
--- main.py
+++ main.py
@@ -1,75 +1,149 @@
-# main.py
-
 import os
-import sys
-import argparse
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.core.window import Window
+from kivy.uix.popup import Popup
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.filechooser import FileChooserListView
+from kivy.uix.image import Image
 from dotenv import load_dotenv
-from app import __version__
-from utils.__env import *
-from utils.conf import load_config
-from utils.file import create_output_directory, get_supported_files
-from utils.preprocess import preprocess_media
 
 """
-Parse command-line arguments
+Definition of Kivy application
 """
-def create_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(description="PreProcessing")
-    parser.add_argument(
-        "-t",
-        "--type",
-        type=str,
-        default="image",
-        choices=["image", "i", "video", "v"],
-        help="Specify the data type: 'image(i)' or 'video(v)' (default: 'image')"
-    )
-    parser.add_argument(
-        "-p",
-        "--path",
-        type=str,
-        default="./assets",
-        help="Path to images or video files (default: './assets')"
-    )
-    parser.add_argument(
-        "-c",
-        "--config",
-        default="./config.yaml",
-        type=str,
-        help="Path to the configuration file (default: './config.yaml')"
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        default="./result",
-        type=str,
-        help="Path to the output directory (default: './result')"
-    )
-    return parser
+class CvApp(App):
+    def build(self):
+        # Set window size
+        Window.size = tuple(map(int, os.getenv('WINDOW_SIZE').strip('[]').split(',')))
+        return MyBoxLayout()
 
 """
-Main function
+Custom layout
 """
-def main(args: argparse.Namespace) -> None:
-    # App information
-    print(f"==================================")
-    print(f"App version: {__version__}")
-    print(f"Python version: {sys.version}")
-    print(f"==================================")
+class MyBoxLayout(BoxLayout):
+    def __init__(self, **kwargs):
+        super(MyBoxLayout, self).__init__(**kwargs)
+        self.bind(on_parent=self.on_parent)
 
-    # Load configuration and setup output directory
-    config_data = load_config(args.config)
-    output_directory = args.output
-    create_output_directory(output_directory)
-    data_type = args.type
-    input_path = args.path
-    # Get supported files for preprocessing
-    supported_files = get_supported_files(input_path, data_type)
+    def on_parent(self, instance, value):
+        if value:
+            left_box = self.ids.left_box
+            if left_box:
+                left_box.bind(on_size=self.update_size_hint_x)
 
-    # Preprocess the files
-    if supported_files:
-        media_settings = config_data.get("image_settings", {}) if data_type in ["image", "i"] else config_data.get("video_settings", {})
-        preprocess_media(supported_files, output_directory, media_settings)
+    def update_size_hint_x(self, instance, value):
+        left_box = self.ids.left_box
+        if left_box:
+            left_box.size_hint_x = instance.width / self.width
 
-if __name__ == "__main__":
-    command_args = create_parser().parse_args()
-    main(command_args)
+"""
+Top navigation bar
+"""
+class TopNavBar(BoxLayout):
+    pass
+
+"""
+Control buttons
+"""
+class LeftBox(BoxLayout):
+    pass
+
+"""
+Image display
+"""
+class CenterBox(BoxLayout):
+    pass
+
+"""
+Options for preprocessing
+"""
+class RightBox(BoxLayout):
+    def update_label_text(self, button_text):
+        self.ids.right_label.text = f"{button_text}"
+
+"""
+User button
+"""
+class MyAppButton(Button):
+    def __init__(self, **kwargs):
+        super(MyAppButton, self).__init__(**kwargs)
+        self.bind(on_release=self.on_button_release)
+        self.filters = os.getenv('IMAGE_EXT', '').split()
+
+    # Method called on button click
+    def on_button_release(self, instance):
+        button_text = self.text
+        actions = {
+            'Open': self.show_file_chooser,
+            'Open Dir': self.open_directory,
+            'Next': self.next_action,
+            'Prev': self.prev_action,
+            'Save Dir': self.save_directory,
+            'Save': self.save_action,
+        }
+        action = actions.get(button_text, self.update_right_label)
+        action()
+
+    # Display file chooser
+    def show_file_chooser(self):
+        file_chooser = FileChooserListView(on_submit=self.on_file_chosen)
+        file_chooser.filters = self.filters
+        # Create and display popup containing file chooser
+        popup = Popup(title="Select File", content=file_chooser, size_hint=(0.8, 0.8))
+        popup.open()
+
+    # Callback called on file selection
+    def on_file_chosen(self, instance, value, touch):
+        app = App.get_running_app()
+        center_box = app.root.ids.center_box
+        center_box.clear_widgets()
+
+        if isinstance(instance, FileChooserListView) and value:
+            file_path = value[0]
+
+            try:
+                image_widget = Image(source=file_path)
+                center_box.add_widget(image_widget)
+            except Exception as e:
+                center_box.add_widget(Label(text=f"Error: {str(e)}"))
+
+            parent = instance.parent
+            while parent and not isinstance(parent, Popup):
+                parent = parent.parent
+            if parent:
+                parent.dismiss()
+        else:
+            instance.dismiss()
+    
+    # To be implemented according to needed functionalities
+    def open_directory(self):
+        pass
+
+    def next_action(self):
+        pass
+
+    def prev_action(self):
+        pass
+
+    def save_directory(self):
+        pass
+
+    def save_action(self):
+        pass
+
+    # Update default label
+    def update_right_label(self):
+        app = App.get_running_app()
+        app.root.ids.right_box.update_label_text(self.text)
+
+if __name__ == '__main__':
+    # Load environment variables from .env file
+    try:
+        load_dotenv()
+    except FileNotFoundError:
+        print("Could not find the .env file. Environment variables are not loaded.")
+    
+    # Run the application
+    CvApp().run()
